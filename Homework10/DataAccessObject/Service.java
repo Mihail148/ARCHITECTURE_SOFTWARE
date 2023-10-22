b438402b6b1f438d7441c6c20da727a10e091563
Mihail Rkitcky <papi.pay@mail.ru>
2023-10-22 20:08:27 +0300

    Домашнее задание №10

---
+++ Homework10/DataAccessObject/Service.java
@@ -0,0 +1,16 @@
+package Homework10.DataAccessObject;
+
+import lombok.AllArgsConstructor;
+
+import java.util.List;
+
+@org.springframework.stereotype.Service
+@AllArgsConstructor
+
+public class Service {
+    private Repository repository;
+
+    protected List<String> getProductName(String name) {
+        return repository.getProductName(name);
+    }
+}
---
+++ Homework10/DataAccessObjectApp/DataAccessObjectApplicationTest.java
@@ -0,0 +1,12 @@
+package Homework10.DataAccessObjectApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+
+class DataAccessObjectApplicationTest {
+    @Test
+    void contextLoads() {
+    }
+}
